63c07fdb7a16f099006b0912997a0c179ce63534
ruthgrace <ruthgracewong@gmail.com>
2016-01-16 00:54:34 -0500

    added run trinity

---
+++ run_trinity.sh
@@ -0,0 +1,10 @@
+dir=$1; #first parameter is the directory of all the fastq files
+
+for F in `find $dir -name *.fastq`
+do
+  sample=$(basename $F);
+  sample=${sample::-6};
+  echo "sample $sample for file $F";
+  Trinity --seqType fq --max_memory 20G --single $F --output $sample --trimmomatic --CPU 10;
+done
+
