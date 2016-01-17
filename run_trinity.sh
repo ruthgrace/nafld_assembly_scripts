b51ad590f1e81ebebcd2c553f65be8833a3823ab
ruthgrace <ruthgracewong@gmail.com>
2016-01-17 11:21:56 -0500

    updated trinity assembly script and readme

--- run_trinity.sh
+++ run_trinity.sh
@@ -5,6 +5,6 @@ do
   sample=$(basename $F);
   sample=${sample::-6};
   echo "sample $sample for file $F";
-  Trinity --seqType fq --max_memory 20G --single $F --output $sample --trimmomatic --CPU 10;
+  Trinity --seqType fq --max_memory 20G --single $F --output "trinity_$sample" --trimmomatic --CPU 10;
 done
 
